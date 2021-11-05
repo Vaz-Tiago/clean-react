97cdff8d4a3ef31ce1463ca1153266179da1dea7
Tiago Vaz <Vaz-Tiago>
2021-11-04 22:58:43 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/protocols/Http/http-response.ts
+++ src/data/protocols/Http/http-response.ts
@@ -1,6 +1,14 @@
 export enum HttpStatusCode {
+  ok = 200,
+  created = 201,
+  accepted = 202,
   noContent = 204,
-  unauthorized = 401
+  badRequest = 400,
+  unauthorized = 401,
+  forbidden = 403,
+  notFound = 404,
+  methodNotAllowed = 405,
+  internalServerError = 500,
 }
 
 export type HttpResponse = {
